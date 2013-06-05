d023d5570fe2c4e1f22236a94360d1eafd428f22
Alex Clemmer <clemmer.alexander@gmail.com>
2013-06-05 15:53:39 -0400

    Add license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject mahoutinaction "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Straight Clojure port of examples from Mahout in Action"
+  :url "http://www.github.com/hausdorff/mahout"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.mahout/mahout-core "0.7"]]
   :main mahoutinaction.core)
