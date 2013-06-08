7505399711d2e485625417514498557180d25736
Alex Clemmer <clemmer.alexander@gmail.com>
2013-06-08 15:39:28 -0400

    Put separate functions into one function as they are in original

--- src/mahoutinaction/ch14.clj
+++ src/mahoutinaction/ch14.clj
@@ -3,6 +3,7 @@
            [java.io StringReader]
            [java.util ArrayList]
            [java.util Arrays]
+           [java.util Collections]
            [java.util TreeMap]
            [org.apache.lucene.analysis TokenStream]
            [org.apache.lucene.analysis.tokenattributes TermAttribute]
@@ -13,6 +14,43 @@
            [org.apache.mahout.math SequentialAccessSparseVector]
            [org.apache.mahout.vectorizer.encoders StaticWordValueEncoder
             ConstantValueEncoder Dictionary]))
+
+
+(def features 10000)
+
+(defn classify-ex
+  []
+  (let [traceDictionary (TreeMap.)
+        encoder         (StaticWordValueEncoder. "body")
+        bias            (ConstantValueEncoder. "Intercept")
+        lines           (ConstantValueEncoder. "Lines")
+        analyzer        (StandardAnalyzer. (Version/LUCENE_31))
+        numFeatures       10000
+        learningAlgorithm (OnlineLogisticRegression. 20 numFeatures (L1.))
+        base       (File. "resources/20news-bydate-train/")
+        files      (ArrayList.)
+        newsGroups (Dictionary.)
+        averageLL        0.0
+        averageCorrect   0.0
+        averageLineCount 0.0
+        k                0
+        step             0.0
+        bumps            [1 2 5]
+        ]
+    (.setProbes encoder 2)
+    (.setTraceDictionary encoder traceDictionary)
+    (.setTraceDictionary bias traceDictionary)
+    (.setTraceDictionary lines traceDictionary)
+    (.alpha learningAlgorithm 1)
+    (.decayExponent learningAlgorithm 0.9)
+    (.lambda learningAlgorithm 3.0e-5)
+    (.learningRate learningAlgorithm 20)
+    (doseq [newsgroup (.listFiles base)]
+      (.intern newsGroups (.getName newsgroup))
+      (.addAll files (Arrays/asList (.listFiles newsgroup))))
+    (Collections/shuffle files)
+    (printf "%d training files\n", (.size files))
+    ))
 
 (defn tokenizing-and-vectorizing-text
   "tokenizes, vectorizes, and prints a piece of text"
@@ -28,34 +66,3 @@
         w          (String. termBuffer 0 termLen)]
     (.addToVector encoder w 1.0 v1)
     (printf "%s\n" (SequentialAccessSparseVector. v1))))
-
-(defn setting-up-vector-encoders
-  "converts text and line counts into vector values"
-  []
-  (let [traceDictionary (TreeMap.)
-        encoder         (StaticWordValueEncoder. "body")
-        bias            (ConstantValueEncoder. "Intercept")
-        lines           (ConstantValueEncoder. "LInes")
-        analyzer        (StandardAnalyzer. (Version/LUCENE_31))]
-    (.setProbes encoder 2)
-    (.setTraceDictionary encoder traceDictionary)
-    (.setTraceDictionary bias traceDictionary)
-    (.setTraceDictionary lines traceDictionary)))
-
-(defn configuring-the-learning-algorithm
-  [numFeatures 10000]
-  (let [learningAlgorithm (OnlineLogisticRegression. 20 numFeatures (L1.))]
-    (.alpha learningAlgorithm 1)
-    (.decayExponent learningAlgorithm 0.9)
-    (.lambda learningAlgorithm 3.0e-5)
-    (.learningRate learningAlgorithm 20)))
-
-(defn accessing-data-files
-  []
-  (let [base       (File. "resources/20news-bydate-train/")
-        files      (ArrayList.)
-        newsGroups (Dictionary.)]
-    (for [newsgroup (.listFiles base)]
-      (do
-       (.intern newsGroups (.getName newsgroup))
-       (.addAll files (Arrays/asList (.listFiles newsgroup)))))))
